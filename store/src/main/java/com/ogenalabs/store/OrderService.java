b6ce7ae73f2f00a09f39ee64664b6983f088f000
David Parseen Maitoyo <david.parseen@gmail.com>
2025-10-24 08:34:24 +0000

    feat: Add overloaded constructor to OrderService for additional payment service initialization

--- store/src/main/java/com/ogenalabs/store/OrderService.java
+++ store/src/main/java/com/ogenalabs/store/OrderService.java
@@ -1,11 +1,18 @@
 package com.ogenalabs.store;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderService {
     private PaymentService paymentService;
-    
+
+    public OrderService(PaymentService paymentService, int x){}
+
+    @Autowired
+    public OrderService(PaymentService paymentService){
+        this.paymentService = paymentService;
+    }
     public void placeOrder(){
         paymentService.processPayment(10);
     }
