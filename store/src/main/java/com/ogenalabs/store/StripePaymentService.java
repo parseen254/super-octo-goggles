de82aa5e70ce8507baf99571a206a1a7f8384531
David Parseen Maitoyo <david.parseen@gmail.com>
2025-10-24 09:22:03 +0000

    feat: Update OrderService to use StripePaymentService; add configuration properties for Stripe API

--- store/src/main/java/com/ogenalabs/store/StripePaymentService.java
+++ store/src/main/java/com/ogenalabs/store/StripePaymentService.java
@@ -1,15 +1,23 @@
 package com.ogenalabs.store;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 @Service("stripe")
 @Primary
 public class StripePaymentService implements PaymentService {
+    @Value("${stripe.apiUrl}")
+    private String apiUrl;
+
+    @Value("${stripe.timeout:3000}")
+    private Integer timeout;
 
     @Override
     public void processPayment(double amount) {
         System.out.println("STRIPE");
         System.out.println("Amount: " + amount);
+        System.out.println("apiUrl: " + apiUrl);
+        System.out.println("timeout: " + timeout);
     }
 }
