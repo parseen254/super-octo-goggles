e2bc1ec563ced0917ba93b9f82b51d0e492c949c
David Parseen Maitoyo <david.parseen@gmail.com>
2025-10-24 07:39:12 +0000

    feat: Add VSCode settings and update Spring Boot version; enhance HomeController to log application name

--- store/src/main/java/com/ogenalabs/store/HomeController.java
+++ store/src/main/java/com/ogenalabs/store/HomeController.java
@@ -2,11 +2,17 @@ package com.ogenalabs.store;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Value;
 
 @Controller
 public class HomeController {
+    @Value("${spring.application.name}")
+    private String appName;
+
+
     @RequestMapping("/")
     public String index() {
+        System.out.println("appName: "+appName);
         return "index.html";
     }
 
